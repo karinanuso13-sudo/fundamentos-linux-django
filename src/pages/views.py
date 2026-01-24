0d35660338ece22c85555477a37225fcfa6640c2
Karina Nuñez <karinanuso13@gmail.com>
2026-01-23 19:22:50 -0600

    Remove stray character and fix import formatting

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,12 +5,13 @@ from django.conf import settings
 from django.shortcuts import render
 
 
-
 def home(request):
     context = {
         "debug": settings.DEBUG,
         "django_ver": get_version() + "Proyecto modificado por Karina",
-        "python_ver": os.environ["PYTHON_VERSION"] + "Este texto fue agregado como parte de la práctica de Django",
+        "python_ver": os.environ["PYTHON_VERSION"]
+        + "Este texto fue agregado como parte de la práctica de Django",
     }
 
     return render(request, "pages/home.html", context)
+
